9916339a15e1b5598492a2a7df268260fa18825f
StefanTrsunov <trsunovstefan@gmail>
2021-10-27 22:27:07 +0200

    Added a new leetcode problem

---
+++ leetcode/1. Two sum.py
@@ -0,0 +1,6 @@
+#try to solve this one if you are stuck just ask :D
+# here's a link https://leetcode.com/problems/two-sum/
+#a little bit a help on line 6 :D
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        seen = {}
